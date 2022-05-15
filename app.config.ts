fa2476e3eab33dca0edde69da190cc08c5acca13
nas <y.softvalley@hey.com>
2022-05-16 00:42:50 +0900

    fix ogp

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export const AppConfig = {
   site_name: "sushiware.xyz",
   title: "sushiware.xyz",
   description: "スマートコントラクトにまつわる自身のための灯台",
-  url: "https://app.sushiware.xyz",
+  url: "https://sushiware.xyz",
   locale: "ja",
   twitter: "@sushiware",
   github: "nasjp",
